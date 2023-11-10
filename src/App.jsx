6118df6d62379d93504dccd1ccf0107a3caa084a
badr ghanbi <bghanbi50@gmail.com>
2023-11-10 14:51:57 +0100

    created the buttons componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import Button from "./ui/Button";
 
 const App = () => {
-  return (
-    <div className="text-white">
-      <button className="inline-flex items-center justify-center font-medium  transition-colors  border border-input bg-background hover:bg-accent1 hover:text-white text-sm h-10 px-4 rounded-full">
-        <span className="mr-3 capitalize">get started</span>{" "}
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          aria-hidden="true"
-          class="h-3"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-          ></path>
-        </svg>{" "} */}
-        <span> &gt;</span>
-      </button>
-    </div>
-  );
+  return <h1>hello </h1>;
 };
 
 export default App;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,11 @@
 const Button = ({ children, className }) => {
-  return <button className={` ${className}`}>{children}</button>;
+  return (
+    <button
+      className={`inline-flex items-center justify-center font-medium  transition-colors ${className}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
-
-// text-white  px-4 py-2 capitalize rounded-full text-md font-semibold
