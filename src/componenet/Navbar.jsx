6b2ce2e1db4a6d6993687efbb034a40ed8c3543a
badr ghanbi <bghanbi50@gmail.com>
2023-11-11 16:22:49 +0100

    created the navbar

--- src/componenet/Navbar.jsx
+++ src/componenet/Navbar.jsx
@@ -3,6 +3,7 @@ import menuHamburger from "../assets/menu.svg";
 import moon from "../assets/moon.svg";
 import sun from "../assets/sun.svg";
 import logo from "../assets/logo.svg";
+import Button from "../ui/Button";
 
 const navLinks = ["Sign In", "Blog", "Documentation", "Pricing", "FAQ"];
 const Navbar = () => {
@@ -19,11 +20,14 @@ const Navbar = () => {
   return (
     <nav className=" py-2 px-3  border-b border-gray-50 border-opacity-10 dark:border-dark-800/70">
       <div className="container mx-auto flex justify-between items-center">
-        <img src={logo} alt="malerkit logo" />
+        <div className="w-[80%] lg:w-auto">
+          <img src={logo} alt="malerkit logo" />
+        </div>
 
         {/* dark and light mode togglers */}
-        <div className="flex items-center">
-          <div className="relative mr-10">
+        {/* <div className="flex items-center w-1/2 "> */}
+        <div className="lg:flex lg:items-center lg:order-2">
+          <div className="relative  mr-10 ">
             <img
               className="h-5 cursor-pointer"
               onClick={toggleMode}
@@ -31,7 +35,7 @@ const Navbar = () => {
               alt="moon icon"
             />
             <div
-              className={`lg:flex text-white ${
+              className={` text-white ${
                 isModeCardOpen
                   ? "block  shadow-pinkBoxShadow border border-gray-50 border-opacity-10 py-4 p-2 rounded-md absolute top-14 -right-3"
                   : "hidden"
@@ -47,39 +51,49 @@ const Navbar = () => {
                 <span className=" text-sm">dark</span>
               </div>
             </div>
+            {/* BUTTONS */}
           </div>
-
-          {/* Menu icon for smaller screens */}
-          <div className="lg:hidden relative">
-            <img
-              onClick={toggleMenu}
-              className="h-9 cursor-pointer"
-              src={menuHamburger}
-              alt="menu hamburger icon"
-            />
-            {/* Menu items */}
-            <div
-              className={`lg:flex ${
-                isMenuOpen
-                  ? "block  shadow-pinkBoxShadow border border-gray-50 border-opacity-10 p-6 px-8 rounded-md absolute top-14 -right-3"
-                  : "hidden"
-              }`}
-            >
-              <ul>
-                {navLinks.map((navLink) => (
-                  <li
-                    key={navLink.id}
-                    className="p-1 lg:px-2.5 text-sm  font-medium  rounded-md  text-gray-600 dark:text-gray-300"
-                  >
-                    <a href="#" className="text-white">
-                      {navLink}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+          <div className="hidden lg:flex">
+            <Button className="text-white mx-4 hover:bg-accent1 px-6 py-2 rounded-full">
+              Sign In
+            </Button>
+            <Button className="bg-darkPink rounded-full text-white px-6 py-2">
+              Sign Up
+            </Button>
           </div>
         </div>
+
+        {/* Menu icon for smaller screens */}
+        <div className=" relative">
+          <img
+            onClick={toggleMenu}
+            className="h-9 cursor-pointer lg:hidden"
+            src={menuHamburger}
+            alt="menu hamburger icon"
+          />
+          {/* Menu items */}
+          <div
+            className={`lg:flex ${
+              isMenuOpen
+                ? "block  shadow-pinkBoxShadow border border-gray-50 border-opacity-10 p-6 px-8 rounded-md absolute top-14 -right-3"
+                : "hidden"
+            }`}
+          >
+            <ul className="lg:flex">
+              {navLinks.map((navLink) => (
+                <li
+                  key={navLink.id}
+                  className="p-1 lg:px-2.5 text-sm  font-medium  rounded-md  text-gray-600 dark:text-gray-300"
+                >
+                  <a href="#" className="text-white">
+                    {navLink}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        {/* </div> */}
       </div>
     </nav>
   );
