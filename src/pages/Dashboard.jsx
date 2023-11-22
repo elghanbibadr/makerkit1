d9b8515714e2f22d66b13c9be0ed48e21016588a
badr ghanbi <bghanbi50@gmail.com>
2023-11-22 10:12:33 +0100

    import some needed icons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,87 +1,9 @@
 import Navbar from "../componenet/Home/Navbar";
 import { useState } from "react";
-import menu from "../assets/menu.svg";
-import organizationIcon from "../assets/organization.svg";
-import Button from "../ui/Button";
-const navLinks = [
-  "Your Organizations",
-  "Blog",
-  "Documentation",
-  "Pricing",
-  "FAQ",
-];
 
 const Dashboard = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  return (
-    <div className="">
-      <div className="flex block md:hidden justify-between">
-        <div className="flex items-center">
-          <img
-            onClick={() => setIsMenuOpen((prv) => !prv)}
-            className="h-10"
-            src={menu}
-            alt="menu hamburger"
-          />
-          <h3 className="text-white">Dashboard</h3>
-        </div>
-        <Button className="button-transparent text-white rounded-md ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-            class="w-4 mr-2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-            ></path>
-          </svg>
-          <span>Add Widget</span>
-        </Button>
-        <div
-          className={`lg:flex ${
-            isMenuOpen
-              ? "block  shadow-pinkBoxShadow border border-gray-50 border-opacity-10 p-6 px-8 rounded-md absolute top-20 -left-3 w-full"
-              : "hidden"
-          }`}
-        >
-          <ul className="lg:flex">
-            {navLinks.map((navLink) => (
-              <li
-                key={navLink.id}
-                className="p-1 flex items-center gap-2 mb-3 lg:px-2.5 text-sm  font-medium  rounded-md  text-gray-600 dark:text-gray-300"
-              >
-                <img className="h-6 mr-2" src={organizationIcon} alt="" />
-                <a href="#" className="text-white">
-                  {navLink}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      {/* sidebar */}
-      <aside className="hidden md:block">
-        <ul className="text-white">
-          <li className="flex items-center">
-            <span className="font-semibold inline-flex justify-center items-center text-xl text-white bg-blue-600 rounded-full h-7 w-7">
-              B
-            </span>
-            <p className="text-white font-medium text-sm ml-2">badr</p>
-          </li>
-          <li>dashboard</li>
-          <li>setting</li>
-          <li>task</li>
-        </ul>
-      </aside>
-    </div>
-  );
+  return <h1>dashboard</h1>;
 };
 
 export default Dashboard;
