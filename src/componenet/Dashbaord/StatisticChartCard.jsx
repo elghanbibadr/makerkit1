4554eb8059960833cb06e964215b5b9a25472826
badr ghanbi <bghanbi50@gmail.com>
2023-11-24 17:38:51 +0100

    check if the isGrowing property is undefined on an chartcarditems and give the box contains the arrow with the grow value a yellow color

--- src/componenet/Dashbaord/StatisticChartCard.jsx
+++ src/componenet/Dashbaord/StatisticChartCard.jsx
@@ -7,6 +7,8 @@ const StatisticChartCard = ({
   gaineValue,
   chartLineData,
 }) => {
+  const isUndefined = typeof isGrowing === "undefined";
+  //   #eab3081a
   return (
     <div className=" border border-accent1 p-4 rounded-md">
       <h4 className="text-gray-400 font-medium text-md">{chartCardTitle}</h4>
@@ -14,7 +16,15 @@ const StatisticChartCard = ({
         <span className="text-white text-2xl md:text-4xl font-bold">
           {gaineValue}
         </span>
-        <div className="bg-[#22c55e1a] flex items-center w-fit font-semibold px-3 py-2 rounded-md p-1">
+        <div
+          className={`${
+            isUndefined
+              ? "bg-[#eab3081a]"
+              : isGrowing
+              ? "bg-[#22c55e1a] "
+              : "bg-[#ef44441a]"
+          } flex items-center w-fit font-semibold px-3 py-2 rounded-md p-1`}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -22,7 +32,13 @@ const StatisticChartCard = ({
             stroke-width="1.5"
             stroke="currentColor"
             aria-hidden="true"
-            className={`h-4  ${isGrowing ? "text-green-500" : "text-red-500"}`}
+            className={`h-4 ${
+              isUndefined
+                ? "text-yellow-500"
+                : isGrowing
+                ? "text-green-500"
+                : "text-red-500"
+            }`}
           >
             <path
               stroke-linecap="round"
@@ -30,7 +46,15 @@ const StatisticChartCard = ({
               d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
             ></path>
           </svg>
-          <span className="text-green-400 ml-1 text-xs  md:text-sm">
+          <span
+            className={`${
+              isUndefined
+                ? "text-yellow-500"
+                : isGrowing
+                ? "text-green-500"
+                : "text-red-500"
+            }  ml-1 text-xs  md:text-sm`}
+          >
             {percentage}
           </span>
         </div>
