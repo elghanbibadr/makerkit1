da05ff7cfcac310beb47c2ed1a61e61c59331345
badr ghanbi <bghanbi50@gmail.com>
2023-11-24 16:17:34 +0100

    created the chart carard

--- src/ui/LineChart.jsx
+++ src/ui/LineChart.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import arrow from "../assets/arrow.svg";
 import { Line } from "react-chartjs-2";
 
 import {
@@ -60,36 +58,4 @@ const LineChart = () => {
   return <Line data={data} options={options} />;
 };
 
-const Dashboard = () => {
-  return (
-    <div className="w-[400px] border border-accent1 p-4 rounded-md">
-      <h4 className="text-gray-400 font-medium text-md">
-        Monthly Recurring Revenue
-      </h4>
-      <div className="flex justify-between my-4">
-        <span className="text-white text-2xl md:text-4xl font-bold">$8.5</span>
-        <div className="bg-[#22c55e1a] flex items-center w-fit font-semibold px-3 py-2 rounded-md p-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-            class="h-4 text-green-500"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
-            ></path>
-          </svg>
-          <span className="text-green-400 ml-1 text-sm">20%</span>
-        </div>
-      </div>
-      <LineChart />
-    </div>
-  );
-};
-
-export default Dashboard;
+export default LineChart;
