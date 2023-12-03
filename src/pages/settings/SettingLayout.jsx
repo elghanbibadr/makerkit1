1210a8e22d41b84d8953e432c61b59b1676427cc
badr ghanbi <bghanbi50@gmail.com>
2023-12-03 20:30:59 +0100

    created a ul list to navigate bewteen oages on the settings pages

--- src/pages/settings/SettingLayout.jsx
+++ src/pages/settings/SettingLayout.jsx
@@ -4,12 +4,16 @@ const SettingLayout = () => {
   return (
     <div>
       <h1>setting</h1>
-      <ul className="text-white">
-        <li>
+      <ul className="text-white flex w-full justify-between md:flex-auto md:justify-normal mb-6 border-accent1 border-b-[1px] pb-3   items-center gap-x-10">
+        <li className="text-[1rem] capitalize font-medium">
           <Link to="profil">profil</Link>
         </li>
-        <li>organization</li>
-        <li>subscription</li>
+        <li className="text-[1rem] capitalize font-medium">
+          <Link to="organization">organization</Link>
+        </li>
+        <li className="text-[1rem] capitalize font-medium">
+          <Link to="subscription">subscription</Link>
+        </li>
       </ul>
       <Outlet />
     </div>
