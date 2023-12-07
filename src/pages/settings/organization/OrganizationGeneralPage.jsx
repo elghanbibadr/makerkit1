8b926c919e7ff43063e888eb75243797689cc0a9
badr ghanbi <bghanbi50@gmail.com>
2023-12-07 10:01:59 +0100

    created the content for the organization general page

--- src/pages/settings/organization/OrganizationGeneralPage.jsx
+++ src/pages/settings/organization/OrganizationGeneralPage.jsx
@@ -1,3 +1,5 @@
+import Button from "../../../ui/Button";
+
 const OrganizationGeneralPage = () => {
   return (
     <div>
@@ -7,6 +9,32 @@ const OrganizationGeneralPage = () => {
           General Manage your Organization
         </p>
       </div>
+      <div className="mt-4">
+        <label className="small-title " htmlFor="organizationName">
+          Organization Name
+        </label>
+        <input
+          className="input block w-full "
+          id="organizationName"
+          name="organizationName"
+          type="text"
+        />
+      </div>
+
+      <div className="mt-6">
+        <label className="small-title " htmlFor="organizationlogo">
+          Organization Logo
+        </label>
+        <input
+          className="input block w-full "
+          id="organizationlogo"
+          name="organizationlogo"
+          type="file"
+        />
+      </div>
+      <Button className="bg-darkPink mt-5 text-white p-3 rounded-md text-sm">
+        Update Organization
+      </Button>
     </div>
   );
 };
--- src/pages/settings/profil/ProfilPasswordPage.jsx
+++ src/pages/settings/profil/ProfilPasswordPage.jsx
@@ -1,3 +1,5 @@
+import Button from "../../../ui/Button";
+
 const ProfilPasswordPage = () => {
   return (
     <div>
@@ -7,6 +9,32 @@ const ProfilPasswordPage = () => {
           Update your password
         </p>
       </div>
+      <div className="mt-4">
+        <label className="small-title " htmlFor="newpassword">
+          Your New Password
+        </label>
+        <input
+          className="input block w-full "
+          id="newpassword"
+          name="newpassword"
+          type="password"
+        />
+      </div>
+
+      <div className="mt-6">
+        <label className="small-title " htmlFor="repeatedpassword">
+          Repeat Password
+        </label>
+        <input
+          className="input block w-full "
+          id="repeatedpassword"
+          name="repeatedpassword"
+          type="password"
+        />
+      </div>
+      <Button className="bg-darkPink mt-5 text-white p-3 rounded-md text-sm">
+        Update Password Address
+      </Button>
     </div>
   );
 };
