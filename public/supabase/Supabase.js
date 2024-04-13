0068721997a024144de830d44c715a851bca7654
elghanbibadr <bghanbi50@gmail.com>
2024-04-13 09:43:28 +0200

    fix conflicts

--- public/supabase/Supabase.js
+++ public/supabase/Supabase.js
@@ -1,14 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://sduwahyntemsearaxrzg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkdXdhaHludGVtc2VhcmF4cnpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEzNzA4NjAsImV4cCI6MjAxNjk0Njg2MH0.h43bCaVZdmUzcedfRB6nxGbHp4omVVe1-O27_JDJrQI";
+// export const supabaseUrl = "https://sduwahyntemsearaxrzg.supabase.co";
+export const supabaseUrl = 'https://bqasptncljkfogihjmhu.supabase.co'
+const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxYXNwdG5jbGprZm9naWhqbWh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5MTEyNTcsImV4cCI6MjAyODQ4NzI1N30.i8zL_YDmrph94E6cplf_3v8OSyVlop85Jm-m4JuIDpg";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of f94f37a (fix task due date issue)
-=======
->>>>>>> parent of f94f37a (fix task due date issue)
