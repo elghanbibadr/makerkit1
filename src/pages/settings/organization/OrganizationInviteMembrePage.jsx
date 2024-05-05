524e4acf34cf2b4fee90e56f83133e9f6113c60f
elghanbibadr <bghanbi50@gmail.com>
2024-05-05 20:54:13 +0200

    sending invite via email successfully

--- src/pages/settings/organization/OrganizationInviteMembrePage.jsx
+++ src/pages/settings/organization/OrganizationInviteMembrePage.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Input from '../../../ui/Input'
 import Button from '../../../ui/Button'
 import { useState } from 'react'
 import { useInviteMembre } from '../../../hook/useInviteMembre'
 import arrow from '../../../assets/arrow.svg'
 import { Link, useNavigate } from 'react-router-dom'
+import emailjs from '@emailjs/browser';
+
 
 const OrganizationInviteMembrePage = () => {
   const [memberEmail,setOrgEmail]=useState('')
   const [memberRole,setMembreRole]=useState('membre')
  const { inviteMembre, isLoading:isInvitingMember }=useInviteMembre()
  const navigate = useNavigate();
+
+  const form=useRef()
 
   function inviteMembreHandler(e){
    e.preventDefault()
@@ -20,15 +24,18 @@ const OrganizationInviteMembrePage = () => {
     memberRole,
     memberEmail,
     orgId:"1a52b845-58b1-4e08-a0cd-590cf886e11c",
-    to_email:'bghanbi50@gmail.com'
+    // to_email:'bghanbi50@gmail.com'
   }
   console.log(invitedMembre)
-  inviteMembre(invitedMembre)
+  // inviteMembre(invitedMembre)
 
   // Sending email to invited members
+
+  console.log('form current',form.current)
   emailjs
-  .sendForm('service_klj723p', 'template_uw0pu1c',invitedMembre, {
+  .sendForm('service_klj723p', 'template_uw0pu1c',form.current, {
     publicKey: 'IglxKjApNUagHGcdh',
+
   })
   .then(
     () => {
@@ -54,10 +61,11 @@ const OrganizationInviteMembrePage = () => {
       Invite members to your organization
       </p>
     </div>
-    <form onSubmit={inviteMembreHandler} className="my-7">
+    {/* ref={form} */}
+    <form ref={form} onSubmit={inviteMembreHandler} className="my-7">
       <div className="sm:flex  gap-2 ">
-      <Input  value={memberEmail} onChange={(e)=>setOrgEmail(e.target.value)} type='email' placeholder="membre@email.com" required/>
-      <select  id="" className="h-fit relative top-4" value={memberRole} onChange={(e)=> setMembreRole(e.target.value)}>
+      <input name='memberEmail' value={memberEmail} onChange={(e)=>setOrgEmail(e.target.value)} type='email' placeholder="membre@email.com" required/>
+      <select name='memberRole'  id="" className="h-fit relative top-4" value={memberRole} onChange={(e)=> setMembreRole(e.target.value)}>
           <option value="member">member</option>
           <option value="admin">admin</option>
         </select>
