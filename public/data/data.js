3321b1c007873414ea9c2217aca2c2c59ce58a77
elghanbibadr <bghanbi50@gmail.com>
2024-05-19 12:48:17 +0200

    refactored the faq page typorpahy and spacing

--- public/data/data.js
+++ public/data/data.js
@@ -16,3 +16,38 @@ export const linksListsData = [
       items: ["Terms of Service", "Privacy Policy", "Cookie Policy"]
     }
   ];
+
+
+
+
+// FAQ DATA
+export const faqData = [
+  {
+    question: "Do you offer a free trial?",
+    answer:
+      "Yes, we offer a 14-day free trial. You can cancel at any time during the trial period and you won't be charged.",
+  },
+  {
+    question: "Can I cancel my subscription?",
+    answer: "Yes, you can cancel your subscription at any time.",
+  },
+  {
+    question: "Where can I find my invoices?",
+    answer:
+      "You can find your invoices in the billing section of your account.",
+  },
+  {
+    question: "What payment methods do you accept?",
+    answer:
+      "We accept credit cards, PayPal, and other common payment methods.",
+  },
+  {
+    question: "Can I upgrade or downgrade my plan?",
+    answer: "Yes, you can upgrade or downgrade your plan at any time.",
+  },
+  {
+    question: "Do you offer discounts for non-profits?",
+    answer:
+      "Yes, we offer discounts for non-profit organizations. Contact our support for more information.",
+  },
+];
--- src/pages/Faq/FaqPage.jsx
+++ src/pages/Faq/FaqPage.jsx
@@ -1,81 +1,34 @@
 import React, { useState } from "react";
-import arrow from "../../assets/arrow.svg";
-const FaqPage = () => {
-  const faqData = [
-    {
-      question: "Do you offer a free trial?",
-      answer:
-        "Yes, we offer a 14-day free trial. You can cancel at any time during the trial period and you won't be charged.",
-    },
-    {
-      question: "Can I cancel my subscription?",
-      answer: "Yes, you can cancel your subscription at any time.",
-    },
-    {
-      question: "Where can I find my invoices?",
-      answer:
-        "You can find your invoices in the billing section of your account.",
-    },
-    {
-      question: "What payment methods do you accept?",
-      answer:
-        "We accept credit cards, PayPal, and other common payment methods.",
-    },
-    {
-      question: "Can I upgrade or downgrade my plan?",
-      answer: "Yes, you can upgrade or downgrade your plan at any time.",
-    },
-    {
-      question: "Do you offer discounts for non-profits?",
-      answer:
-        "Yes, we offer discounts for non-profit organizations. Contact our support for more information.",
-    },
-  ];
+import arrow from "../../assets/arrow2.svg"
+import { faqData } from "../../../public/data/data";
 
+const FaqPage = () => {
   const [openIndex, setOpenIndex] = useState(null);
 
-  const toggleAnswer = (index) => {
-    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
-  };
+  const toggleAnswer = (index) => setOpenIndex((prevIndex) => (prevIndex === index ? null : index))
+
 
   return (
-    <div className=" w-1/2 mx-auto mt-8">
+    <div className=" md:w-1/2 mx-auto mt-20">
       <div className="text-center my-10">
-        <h2>Pricing</h2>
-        <h3 className="text-gray-600 mt-4 text-2xl font-normal">
+        <h1 className="text-white">FAQ</h1>
+        <h3 className="gradient-text mt-10 mb-16 ">
           Frequently Asked Questions
         </h3>
       </div>
       <div>
         {faqData.map((faq, index) => (
-          <div key={index} className="mb-4 ">
+          <div key={index} className="mb-6 ">
             <div
               className="flex justify-between items-center text-gray-300 hover:text-white cursor-pointer"
               onClick={() => toggleAnswer(index)}
             >
-              <div className="text-lg font-semibold">{faq.question}</div>
-              <div
-                className={`${openIndex === index ? "rotate-180" : "rotate-0"}`}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                  class="h-5 transition duration-300 group-open:-rotate-180"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                  ></path>
-                </svg>
-              </div>
+               <h5 >{faq.question}</h5>
+             
+              <img className={`h-4 ${openIndex === index ? "-rotate-90" : "rotate-90"} duration-300`} src={arrow}  alt="arrow"/>
             </div>
             {openIndex === index && (
-              <p className="text-gray-600 mt-4 text-xl font-normal">
+              <p className=" mt-4 text-small-gray ">
                 {faq.answer}
               </p>
             )}
